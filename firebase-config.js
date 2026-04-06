7f7b1ea10fec5083d1640bba34a001c87b86fd70
kayusejong-blip <kayusejong@gmail.com>
2026-04-06 14:51:40 +0900

    v3.2: Switch to new project zero-medical-cs Firebase Keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyDBtMm0bFTWo7craIuW6pxwWOVIHXYv78A",
-    authDomain: "coupang-master-290fa.firebaseapp.com",
-    databaseURL: "https://coupang-master-290fa-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "coupang-master-290fa",
-    storageBucket: "coupang-master-290fa.firebasestorage.app"
+    apiKey: "AIzaSyDKkr93ANx2TbmWhD1BQNEU4b_rEHEOkyk",
+    authDomain: "zero-medical-cs-260406.firebaseapp.com",
+    databaseURL: "https://zero-medical-cs-260406-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "zero-medical-cs-260406",
+    storageBucket: "zero-medical-cs-260406.firebasestorage.app",
+    messagingSenderId: "255166539730",
+    appId: "1:255166539730:web:c5919a5ab5f04f39d44a0b"
 };
 
 // Initialize Firebase
